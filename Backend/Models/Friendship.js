88a054c38f87223d89769df94e787e589f82ecb9
om-chauhan1 <omchauhan64408@gmail.com>
2024-02-13 21:38:49 +0530

    Created Friends Functionality in Backend

--- Backend/Models/Friendship.js
+++ Backend/Models/Friendship.js
@@ -13,7 +13,7 @@ const friendshipSchema = new mongoose.Schema({
     },
     status: {
         type: String,
-        enum: ['pending', 'accepted', 'rejected'],
+        enum: ['pending', 'accepted'],
         default: 'pending'
     }
 }, {
--- Backend/Routes/friendship.js
+++ Backend/Routes/friendship.js
@@ -3,24 +3,24 @@ const router = express.Router();
 const auth = require("../Middleware/auth");
 const {
     sendFriendReq,
-    // unsendFriendReq,
-    // acceptFriendReq,
-    // rejectFriendReq,
-    // getAllFriends,
-    // removeFriend,
+    cancelFriendReq,
+    acceptFriendReq,
+    rejectFriendReq,
+    getAllFriends,
+    removeFriend,
 } = require("../Controllers/friendship");
 
 // Create
 router.post("/sendFriendRequest", auth, sendFriendReq);
 
 // Read
-// router.get("/friends", auth, getAllFriends);
+router.get("/getAllFriends", auth, getAllFriends);
 
 // Update
-// router.post("/acceptFriendRequest", auth, acceptFriendReq);
-// router.post("/rejectFriendRequest", auth, rejectFriendReq);
+router.post("/acceptFriendRequest", auth, acceptFriendReq);
+router.post("/rejectFriendRequest", auth, rejectFriendReq);
 
 // Delete
-// router.delete("/removeFriend", auth, removeFriend);
-// router.delete("/unsendFriendRequest", auth, unsendFriendReq);
+router.delete("/removeFriend", auth, removeFriend);
+router.delete("/cancelFriendRequest", auth, cancelFriendReq,);
 module.exports = router;
