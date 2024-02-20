5b675174aa407349ab62147b5d39185a2f355ab4
Akshat Dubey <akshatdubey0808@gmail.com>
2024-02-20 21:02:07 +0530

    added state in the user model

--- Backend/Models/User.js
+++ Backend/Models/User.js
@@ -44,6 +44,9 @@ const userSchema = new mongoose.Schema(
       country: {
         type: String,
       },
+      state: {
+        type: String,
+      },
       city: {
         type: String,
       },
