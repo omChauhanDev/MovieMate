cec72eb1f40387aa32f9b124bcba6e48d599989a
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-02-20 22:52:07 +0530

    fixed the login bug

--- frontend/src/store/atoms.js
+++ frontend/src/store/atoms.js
@@ -6,7 +6,8 @@ const userAtom = atomWithStorage("user", {});
 const isDarkAtom = atomWithStorage("isDark", false);
 const isLoggedInAtom = atom(localStorage.getItem("user") !== null);
 const resetLoginAtom = atom(null, (_get, set) => {
-  set(isLoggedInAtom, !isLoggedInAtom);
+  const isLoggedIn = localStorage.getItem("user") !== null;
+  set(isLoggedInAtom, !isLoggedIn);
 });
 
 export { userAtom, isDarkAtom, isLoggedInAtom, resetLoginAtom };
