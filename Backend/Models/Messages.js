2bc91700219d26db0cbf560c41945802c151c896
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-02-23 19:45:46 +0530

    Created conversations and messages models

---
+++ Backend/Models/Messages.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const messagesSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Messages", messagesSchema);
