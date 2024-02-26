877d9092df4a7f0fff85a3230a8ed2043ff85829
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-02-26 09:41:04 +0530

    Added try catch for the Node mailer

--- Backend/Config/NodeMailerTransporter.js
+++ Backend/Config/NodeMailerTransporter.js
@@ -1,11 +1,16 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
-const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-});
+try {
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
+} catch (err) {
+  console.log(err);
+  console.log("Something went wrong with the Mailer...");
+}
 
 module.exports = transporter;
